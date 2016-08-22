546984c4bd259d93362f0ecb41d7d72e8ed125ff
fanyongdong <fanyongdong@xiaomi.com>
2016-08-23 00:30:24 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # benFont
-提取模板关键字，生成字体文件
+
+> 可以针对不同的项目，字体生成使用
+
+### 特点
+
+- 支持同个页面多个字体
+- 支持选择器，过滤生成特定字体
+
+### 安装
+
+```
+git clone https://github.com/fyddaben/benFont.git 
+cd benFont 
+npm install
+```
+## 运行命令
+```
+SRC_ENV=../products/**/*.tpl  node app.js
+```
+`SRC_ENV` 变量可以针对不同的项目进行改变监听路径, 可以是相对或者绝对路径
+
+### 使用
+
+- 例如上面例子，是针对`../product/`下面的任意`tpl`文件，不限层级
+- 还需要在`tpl`文件头部声明，使用字体，及`class`选择器名称，例如
+
+```
+<input type="hidden" class='J_niufont' data-class='niu-font' value='syNormal'>
+```
+
+- `data-class`为选择器名称，可以自定义
+- `value`为字体名称，可以自行更改，但必须与`benFont`的`ttf`文件夹里面的字体名字保持一致(去掉`.ttf`后的名字)
+
+- 在`scss`文件中声明
+
+```
+@mixin fontBlock($name, $fontName) {
+    font-family: $fontName;
+    src: url("./font/" + $name + "_" + $fontName + ".eot");
+    src:
+    url("./font/" + $name + "_" + $fontName + ".eot?#font-spider") format('embedded-opentype'),
+    url("./font/" + $name + "_" + $fontName + ".woff") format('woff'),
+    url("./font/" + $name + "_" + $fontName + ".ttf") format('truetype'),
+    url("./font/" + $name + "_" + $fontName + ".svg") format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face {
+    @include fontBlock('index', 'DINOffcPro-Cond');
+}
+.niu-num-font{
+    font-family: 'DINOffcPro-CondBold';
+}
+```
+- `$name` 表示模板的名字，字体文件跟着模板生成的，所以需要传入模板的名字
+- `$fontName` 表示`ttf`文件夹里面的字体名字。然后声明`font-family`的时候也要保持一致
+
+#### 谢谢浏览ヾ(｡´･_●･`｡)☆
---
+++ app.js
@@ -0,0 +1,162 @@
+var fontSrc = './ttf/';
+var fileSrc = process.env.SRC_ENV;
+var chokidar = require('chokidar');
+var Fontmin = require('fontmin');
+var jsdom = require('jsdom');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var log4js = require('log4js');
+var rename = require('gulp-rename');
+
+log4js.loadAppender('file');
+
+log4js.addAppender(log4js.appenders.file('logs/benfont.log'), 'shaobing');
+
+var logger = log4js.getLogger('shaobing');
+
+// 初始化监听器
+var watcher = chokidar.watch(fileSrc, {
+    persistent: true,
+    ignoreInitial: true
+});
+
+
+var changeCallback = function(path) {
+    logger.info(path, ' change or add');
+    checkFontExist(path);
+}
+
+watcher.on('add', changeCallback).on('change', changeCallback);
+
+console.log('Start watch tpl change ');
+
+// 判断font文件夹是否存在
+var checkFontExist = function(path) {
+    var lineLastIndex = path.lastIndexOf('/');
+    var fileName = path.substring(lineLastIndex + 1);
+    if (fileName.lastIndexOf('.') != -1) {
+        fileName = fileName.split('.')[0];
+    }
+    var url =  path.substring(0, lineLastIndex + 1) + 'font';
+
+    fs.access(url, fs.F_OK, function(err) {
+        if (!err) {
+
+            readFileAndFilter(path, url, fileName);
+        } else {
+            // It isn't accessible
+            logger.warn('文件夹font 没有建立');
+
+            mkdirp(url, function (err) {
+                if (err){
+                    logger.error(err);
+                } else {
+                    readFileAndFilter(path, url, fileName);
+                }
+            });
+        }
+    });
+
+}
+
+/**
+ * 读取文件，筛选内容
+ * @param  {filePath} 模板路径
+ * @param {url}    font字体生成文件夹地址
+ * @param {fileName}    文件名字
+ */
+var readFileAndFilter = function(filePath, url, fileName) {
+    var data = fs.readFileSync(filePath, 'utf-8');
+    var jquery = fs.readFileSync("./jquery.js", "utf-8");
+    jsdom.env({
+        html: data,
+        src:[jquery],
+        done: function(errors, window) {
+            if (errors) {
+                logger.error('读取文件错误');
+                logger.error(errors);
+            }
+            var $= window.$;
+
+            var fontObj = $('.J_benfont')
+            var fontSpecs = fontObj.length;
+            // 判断有几种字体
+            if (fontSpecs == 0) {
+                logger.warn('这个文件没有声明字体,请在模板添加一个或多个<input type="hidden" class="J_benfont" data-class="ben-font" value="syNormal">');
+                return false;
+            }
+            fontObj.each(function(i) {
+                var _this = $(this);
+                var _className = _this.attr('data-class');
+                var classAmount = $('.' + _className).length;
+                var content = '';
+                if (classAmount == 0) {
+                    logger.warn('class ' + _className + ' 没有任何声明');
+                } else {
+                    $('.' + _className).each(function() {
+                        content = content + $(this).text();
+                    });
+                    var contentArr = content.split('');
+                    content = unique(contentArr).join('');
+
+                    // 重命名字体名字
+                    var reFontName = fileName + '_' + _this.val();
+                    var fontpath = fontSrc + _this.val() + '.ttf';
+                    fontGen(fontpath, content, url, reFontName);
+                }
+            });
+
+        }
+    });
+}
+
+/**
+ * 生成字体
+ * @param  {fontpath} 字体路径
+ * @param {val}    转换内容
+ * @param {destpath}  字体生成目标文件夹
+ * @param {fontName}  字体名字
+ */
+var fontGen = function(fontpath, val, destpath, fontName) {
+    var fontmin = new Fontmin()
+    .src(fontpath)
+    .use(rename(fontName + '.ttf'))
+    .use(Fontmin.glyph({
+        text: val
+    }))
+    .use(Fontmin.ttf2eot())
+    .use(Fontmin.ttf2woff())
+    .use(Fontmin.ttf2svg())
+    .dest(destpath);
+    fontmin.run(function(err, files) {
+        if (err) {
+            throw err;
+            logger.error('生成字体错误');
+            logger.error(err);
+        } else {
+            logger.info('生成字体 success');
+        }
+    });
+}
+
+/**
+ * 数组去重
+ * @param  {[type]} arr [description]
+ * @return {[type]}     [description]
+ */
+var unique = function(arr) {
+    var result = [],
+        hash = {};
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (!hash[arr[i]]) {
+            result.push(arr[i]);
+            hash[arr[i]] = true;
+        }
+    }
+    return result;
+}
+
+
+
+
+
